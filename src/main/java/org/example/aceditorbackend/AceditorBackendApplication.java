f8f1beb8f3766f9133c373e58a9ab4c42b862a58
Panos <panoslin@foxmail.com>
2024-08-02 15:40:59 -0400

    init project

---
+++ src/main/java/org/example/aceditorbackend/AceditorBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.aceditorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceditorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AceditorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aceditorbackend/AceditorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aceditorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceditorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
