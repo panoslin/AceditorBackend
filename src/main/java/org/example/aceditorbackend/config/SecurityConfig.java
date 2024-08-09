860f68e17ef3657943e31b3e433d2e643b5f2a9b
Panos <panoslin@foxmail.com>
2024-08-09 17:35:07 -0400

    add: custom authentication

--- src/main/java/org/example/aceditorbackend/config/SecurityConfig.java
+++ src/main/java/org/example/aceditorbackend/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package org.example.aceditorbackend.config;
 
+import org.example.aceditorbackend.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +18,20 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+        return authBuilder.build();
+    }
+
 
     @Bean
     @Profile("dev")
--- src/main/java/org/example/aceditorbackend/repository/UserRepository.java
+++ src/main/java/org/example/aceditorbackend/repository/UserRepository.java
@@ -3,7 +3,13 @@ package org.example.aceditorbackend.repository;
 import org.example.aceditorbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
+
 }
